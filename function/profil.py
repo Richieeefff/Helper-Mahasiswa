33ae1462e71c95c8f4e319851959a861ab0a3fa3
richie <ikhsannul201@gamil.com>
2024-12-21 15:11:30 +0700

    add profil

---
+++ function/profil.py
@@ -0,0 +1,42 @@
+from function import login
+
+def calculate_level(username, scaling_factor=100):
+    database = login.load_user_data()
+    for user in database["users"]:
+        if user["username"] == username:
+            exp = user["exp"]
+            return int((exp / scaling_factor) ** 0.5)
+    return 0  
+
+def showProfile(username):
+   
+    while True:
+        database = login.load_user_data()
+
+        
+        user = next((u for u in database["users"] if u["username"] == username), None)
+
+        if not user:
+            print(f"User '{username}' not found.")
+            return
+
+        # Display user profile
+        print("\n--- User Profile ---")
+        print(f"Username: {username}")
+        print(f"Kamu mempunyai {len(user['scheduled_tasks'])} tugas pending.")
+        print(f"Level: {calculate_level(username)}")
+        print("\nOptions:")
+        print("1. Refresh Profile")
+        print("2. Exit")
+
+        
+        choice = input("Pilih degan angka: ")
+        if choice == '1':
+            continue  
+        elif choice == '2':
+            return
+        else:
+            print("Invalid option. Please try again.")
+
+        
+
--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from function import (
     jadwalkuliah,
     timerBelajar,
     tipsBelajar,
-    jadwalTugas
+    jadwalTugas,
+    profil
 )
 
 def interface(username):
@@ -13,22 +14,26 @@ def interface(username):
     while True:
         helper.clear()
         print(f"Selamat datang {username} di aplikasi Helper Mahasiswa!\n")
-        print("1. Timer Belajar")
-        print("2. Atur Jadwal Kuliah")
-        print("3. Atur daftar tugas")
-        print("4. Exit")
+        print("1. profile")
+        print("2. Timer Belajar")
+        print("3. Atur Jadwal Kuliah")
+        print("4. Atur daftar tugas")
+        print("5. Exit")
         choice = input("Pilih dengan angka: ")
 
-        if choice == '1':
+        if choice == "1":
+            helper.clear()
+            profil.showProfile(username)
+        if choice == '2':
             helper.clear()
             timerBelajar.timerSetup()
-        elif choice == '2':
-            helper.clear()
-            jadwalkuliah.main()
         elif choice == '3':
             helper.clear()
-            jadwalTugas.main_tugas(username)
+            jadwalkuliah.main()
         elif choice == '4':
+            helper.clear()
+            jadwalTugas.main_tugas(username)
+        elif choice == '5':
             print("\nTerima kasih telah menggunakan aplikasi Helper Mahasiswa!")
             break
         else:
